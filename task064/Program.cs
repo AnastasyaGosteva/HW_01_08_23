4021acea8d13282545c1b80538e80571c78474c6
Roman & Anastasiya <roma.gostev2017@yandex.ru>
2023-08-05 21:57:07 +0700

    Сделала task064

---
+++ task064/Program.cs
@@ -0,0 +1,8 @@
+﻿string СonclusionNumber(int number)
+{
+    if (number == 1) return number.ToString();
+    return number + ", " + СonclusionNumber(number - 1);
+}
+Console.Write("Задайте значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(СonclusionNumber(n));
